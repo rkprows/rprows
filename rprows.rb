9499cafe3262e4251d35ec129e4984ca34d58aae
Rachel Prows <rachel.prows@saxtonhorne.com>
2017-06-14 10:12:32 -0600

    initial commit

---
+++ rprows.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/projects' do
+  erb :projects
+end
+
+get '/resume' do
+  erb :resume
+end
+
+get '/contact' do
+  erb :contact
+end
+
+
+
+
