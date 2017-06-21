b7659d80611c4070b6f4bd74e9e3c7320e207574
Rachel Prows <rachel.prows@saxtonhorne.com>
2017-06-21 16:18:03 -0600

    basic home page

--- rprows.rb
+++ rprows.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-
+require 'pony'
 get '/' do
   erb :index
 end
@@ -20,6 +20,42 @@ get '/contact' do
   erb :contact
 end
 
+get'/success' do
+  erb :success
+end
+post '/contact' do 
+  configure_pony
+  name = params[:name]
+  sender_email = params[:email]
+  message = params[:message]
+  logger.error params.inspect
+  begin
+    Pony.mail(
+      :from => "#{name}<#{sender_email}>",
+      :to => 'rachel.prows@com',
+      :subject =>"#{name} has contacted you",
+      :body => "#{message}",
+    )
+    redirect '/success'
+  rescue
+    @exception = $!
+    erb :boom
+  end
+end
 
+def configure_pony
+  Pony.options = {
+    :via => :smtp,
+    :via_options => { 
+      :address              => 'smtp.mail.me.com', 
+      :port                 => '587',  
+      :user_name            => ENV['ICLOUD_USERNAME'], 
+      :password             => ENV['ICLOUD_PASSWORD'], 
+      :authentication       => :plain, 
+      :enable_starttls_auto => true,
+      :domain               => 'heroku.com'
+    }    
+  }
+end
 
 
