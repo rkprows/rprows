cb54d31248a5ba2c79cf0ef8318959143623cc24
Rachel Prows <rachel.prows@saxtonhorne.com>
2017-08-02 14:04:59 -0600

    fixed js for bio show/hide, started alternative layout

--- public/index.js
+++ public/index.js
@@ -1,18 +1,41 @@
 var about = document.getElementById("about");
 var show = document.getElementById("show");
 var hide = document.getElementById("hide");
+var intro = document.getElementById("summary");
+var it = document.getElementById('intro-title');
 
 show.onclick = function() {
   show.style.display = 'none';
   about.style.display = 'block';
-  about.style.transition = all 1s ease-out;
+  // about.style.transition = all 1s ease-out;
   hide.style.display = 'block';
+  intro.style.display = 'none';
+  it.style.display = 'none';
 }
 
 hide.onclick = function() {
   about.style.display = 'none';
   hide.style.display = 'none';
-  about.style.transition = all 1s ease-out;
+  // about.style.transition = all 1s ease-out;
   show.style.display = 'block';
+  intro.style.display = 'block';
+  it.style.display = 'block';
 
 }
+
+// const panels = document.querySelectorAll('.panel');
+
+// function toggleOpen() {
+//   console.log('Hello');
+//   this.classList.toggle('open');
+// }
+
+// function toggleActive(e) {
+//   console.log(e.propertyName);
+//   if (e.propertyName.includes('flex')) {
+//     this.classList.toggle('open-active');
+//   }
+// }
+
+// panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+// panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
